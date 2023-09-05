8f870f48717f2f379d58613398129f36e7a08670
Ravi <99581144+ravigithubshankar@users.noreply.github.com>
2023-09-05 19:14:51 +0530

    Create ravi.py

---
+++ ravi.py
@@ -0,0 +1,13 @@
+def func(a):
+    count=0
+    for i in range(len(a)):
+        if a[i]!=0:
+          a[count]=a[i]
+          count+=1
+    for j in range(count,len(a)):
+        a[i]=0
+
+
+a=[0,1,2,3,0,9,0]
+func(a)
+print(a)
